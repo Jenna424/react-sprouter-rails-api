91494bf108302af2a451cef8a593fa6aa23c9d23
github username <github email address>
2019-03-25 20:50:19 -0400

    define #show, #update, #destroy actions in GardensController

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class GardensController < ApplicationController
       before_action :set_garden, only: [:show, :update, :destroy]
-      
+
       def index
         render json: Garden.all
       end
@@ -14,6 +14,26 @@ module Api
         else
           render json: { errors: garden.errors.full_messages }, status: :unprocessable_entity # status: 422
           # render json: { status: 500, error: 'garden was not saved successfully' }
+        end
+      end
+
+      def show
+        render json: @garden, status: 200
+      end
+
+      def update
+        if @garden.update(garden_params)
+          render json: @garden, status: 200
+        else
+          render json: { errors: @garden.errors.full_messages }, status: 400
+        end
+      end
+
+      def destroy
+        if @garden.destroy
+          render json: { message: "Garden was successfully deleted!" }, status: 200
+        else
+          render json: { errors: "Unable to delete garden" }, status: 400
         end
       end
 
