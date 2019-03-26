5fd584e4aa0ae7815ba06de4d8fa1bdd9fdc1722
github username <github email address>
2019-03-25 20:53:23 -0400

    change gardenType to garden_type in GardenSerializer

--- app/serializers/garden_serializer.rb
+++ app/serializers/garden_serializer.rb
@@ -1,5 +1,5 @@
 class GardenSerializer < ActiveModel::Serializer
-  attributes :id, :name, :layout, :gardenType, :plant_ids
+  attributes :id, :name, :layout, :garden_type, :plant_ids
 
   def plant_ids
     object.plants.map(&:id)
