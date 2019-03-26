27ec62f13849d6b8267376bb3cd068911c7a21c3
github username <github email address>
2019-03-25 20:44:31 -0400

    define garden_params and #create action in GardensController

--- app/controllers/api/v1/gardens_controller.rb
+++ app/controllers/api/v1/gardens_controller.rb
@@ -4,6 +4,22 @@ module Api
       def index
         render json: Garden.all
       end
+
+      def create
+        garden = Garden.new(garden_params)
+        if garden.save
+          render json: garden, status: 201 # indicates successful resource creation
+        else
+          render json: { errors: garden.errors.full_messages }, status: :unprocessable_entity # status: 422
+          # render json: { status: 500, error: 'garden was not saved successfully' }
+        end
+      end
+
+      private
+
+        def garden_params
+          params.require(:garden).permit(:name, :layout, :garden_type, plant_ids: [])
+        end
     end
   end
 end
